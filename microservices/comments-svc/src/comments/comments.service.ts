ac5ecd8565468419acec59547b1c3cfc01061902
Benj Sicam <benj.sicam@mi-c3.com>
2020-02-11 11:21:34 +0800

    Bug fix

--- microservices/comments-svc/src/comments/comments.service.ts
+++ microservices/comments-svc/src/comments/comments.service.ts
@@ -14,43 +14,43 @@ export class CommentsServiceImpl implements CommentsService {
   }
 
   async findAll(query?: FindOptions): Promise<Comment[]> {
-    this.logger.info('CommentService#findAll.call', query)
+    this.logger.info('CommentsService#findAll.call', query)
 
     const result = await this.repo.findAll(query)
 
-    this.logger.info('CommentService#findAll.result', result)
+    this.logger.info('CommentsService#findAll.result', result)
 
     return result
   }
 
   async count(query?: FindOptions): Promise<number> {
-    this.logger.info('CommentService#count.call', query)
+    this.logger.info('CommentsService#count.call', query)
 
     const result = await this.repo.count(query)
 
-    this.logger.info('CommentService#count.result', result)
+    this.logger.info('CommentsService#count.result', result)
 
     return result
   }
 
   async create(commentDto: CommentDto): Promise<Comment> {
-    this.logger.info('CommentService#create.call', commentDto)
+    this.logger.info('CommentsService#create.call', commentDto)
 
     const comment = new Comment(commentDto)
 
     const result = await comment.save()
 
-    this.logger.info('CommentService#create.result', result)
+    this.logger.info('CommentsService#create.result', result)
 
     return result
   }
 
   async destroy(query?: FindOptions): Promise<number> {
-    this.logger.info('CommentService#destroy.call', query)
+    this.logger.info('CommentsService#destroy.call', query)
 
     const result = await Comment.destroy(query)
 
-    this.logger.info('CommentService#destroy.result', result)
+    this.logger.info('CommentsService#destroy.result', result)
 
     return result
   }

db5f174cf9a169ac974bc20ffe72940ee75b30f7
Benj Sicam <benj.sicam@mi-c3.com>
2020-02-11 16:53:16 +0800

    Minor enhancement to Database Module

--- microservices/organizations-svc/src/database/database.providers.ts
+++ microservices/organizations-svc/src/database/database.providers.ts
@@ -1,31 +1,30 @@
+import { Provider } from '@nestjs/common'
 import { Sequelize } from 'sequelize-typescript'
 
 import { PinoLogger } from 'nestjs-pino'
 import { Organization } from '../organizations/organization.entity'
 
-export const databaseProviders = [
-  {
-    provide: 'SEQUELIZE',
-    useFactory: async (logger: PinoLogger) => {
-      logger.setContext('Sequelize')
+export const DatabaseProvider: Provider = {
+  provide: 'SEQUELIZE',
+  useFactory: async (logger: PinoLogger) => {
+    logger.setContext('Sequelize')
 
-      const db: Sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || '5432', 10),
-        logging: logger.info.bind(logger),
-        benchmark: true,
-        retry: {
-          max: 3
-        }
-      })
+    const db: Sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      logging: logger.info.bind(logger),
+      benchmark: true,
+      retry: {
+        max: 3
+      }
+    })
 
-      db.addModels([Organization])
+    db.addModels([Organization])
 
-      await db.sync()
+    await db.sync()
 
-      return db
-    },
-    inject: [PinoLogger]
-  }
-]
+    return db
+  },
+  inject: [PinoLogger]
+}
